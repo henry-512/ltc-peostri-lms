b728b0c4a9477964fea2ca0590728394d983421d
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-01-27 16:00:53 -0500

    Fixed types

--- frontend/src/util/types.ts
+++ frontend/src/util/types.ts
@@ -1,70 +1,11 @@
 import { ReduxState } from "react-admin";
 
-export interface LoginInformation {
-     username: string,
-     password: string
-}
+import * as LMSTypes from '../../../lms/types'
+
+export const LMS = LMSTypes;
 
 export type ThemeName = 'light' | 'dark';
 
-export type Status = "IN_PROGRESS" | "COMLETED" | "ARCHIVED" | "AWAITING";
-
 export interface AppState extends ReduxState {
      theme: ThemeName;
-}
-
-export interface IUser {
-     firstName: string;
-     lastName: string;
-     avatar: null | string;
-}
-
-export interface IComment {
-     content: string;
-     author: string | IUser;
-     created: Date;
-     updated: Date;
-}
-
-export interface ITask {
-     title: string;
-     status: Status;
-     assigned?: string | IUser | Array<IUser> | Array<string> | null; //TODO: Should this be a rank instead of a user??
-     comments: Array<IComment>;
-}
-
-export interface ITaskReview extends ITask {
-     type: "DOCUMENT_REVIEW";
-}
-
-export interface ITaskUpload extends ITask {
-     type: "DOCUMENT_UPLOAD";
-}
-
-export interface IModule {
-     title: string;
-     tasks: Array<ITask>;
-}
-
-export interface IProject {
-     title: string;
-     created: Date;
-     updated: Date;
-     startDate: Date;
-     endDate: Date;
-     status: Status;
-     comments: Array<IComment>;
-     modules: Array<IModule>;
-}
-
-export interface IModuleTemplate extends IModule {
-     description: string;
-}
-
-export interface IProjectTemplate {
-     title: string;
-     description: string;
-     created: Date;
-     updated: Date;
-     modules: Array<IModule>;
 }
