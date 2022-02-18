e14f23852e8e7aef894933eb4ec84d6f71064806
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-02-18 02:46:09 -0500

    Frontend deployment

---
+++ frontend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config()
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.APP_PORT);
+console.log("Server is up and running on port: " + process.env.APP_PORT)
