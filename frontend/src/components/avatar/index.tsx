213c36d0a63b41ffefc5838e238a81e3088e98ee
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-03-13 01:16:34 -0500

    Avatar Field for populating user avatars

---
+++ frontend/src/components/avatar/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import { FieldProps } from 'react-admin';
+import { IUser } from 'src/util/types';
+
+interface Props extends FieldProps<IUser> {
+    className?: string;
+    size?: string;
+}
+
+const AvatarField = ({ record, size = '25', className }: Props) =>
+    record ? (
+        <Avatar
+            src={`${record.avatar}?size=${size}x${size}`}
+            style={{ width: parseInt(size, 10), height: parseInt(size, 10) }}
+            className={className}
+        />
+    ) : null;
+
+export default AvatarField;
