c0e1df9bdf408ff147748e00fc90dd75cc4b593d
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-03-17 20:34:11 -0400

    Fixed tabs

--- frontend/src/components/stepper/Stepper.tsx
+++ frontend/src/components/stepper/Stepper.tsx
@@ -4,90 +4,90 @@ import StepHeader from "./StepHeader";
 import StepToolbar from "./StepToolbar"
 
 export default function Stepper(props: any) {
-     const [backText, setBackText] = React.useState("");
-     const [activeStep, setActiveStep] = React.useState(0);
-     const [skipped, setSkipped] = React.useState(new Set());
-     const [validator, setValidator] = React.useState("");
-     const optionalCache: number[] = [];
+    const [backText, setBackText] = React.useState("");
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [skipped, setSkipped] = React.useState(new Set());
+    const [validator, setValidator] = React.useState("");
+    const optionalCache: number[] = [];
 
-     const resetBackText = () => {
-          setBackText("");
-     }
+    const resetBackText = () => {
+        setBackText("");
+    }
 
-     const isStepOptional = (step: number) => {
-          return optionalCache.includes(step);
-     };
+    const isStepOptional = (step: number) => {
+        return optionalCache.includes(step);
+    };
 
-     const setStepOptional = (step: number) => {
-          optionalCache.push(step);
-     }
+    const setStepOptional = (step: number) => {
+        optionalCache.push(step);
+    }
 
-     const isStepSkipped = (step: number) => {
-          return skipped.has(step);
-     };
+    const isStepSkipped = (step: number) => {
+        return skipped.has(step);
+    };
 
-     const handleNext = () => {
-          resetBackText();
-          let newSkipped = skipped;
-          if (isStepSkipped(activeStep)) {
-               newSkipped = new Set(newSkipped.values());
-               newSkipped.delete(activeStep);
-          }
+    const handleNext = () => {
+        resetBackText();
+        let newSkipped = skipped;
+        if (isStepSkipped(activeStep)) {
+            newSkipped = new Set(newSkipped.values());
+            newSkipped.delete(activeStep);
+        }
 
-          setActiveStep((prevActiveStep) => prevActiveStep + 1);
-          setSkipped(newSkipped);
-     };
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        setSkipped(newSkipped);
+    };
 
-     const handleBack = () => {
-          resetBackText();
-          setActiveStep((prevActiveStep) => prevActiveStep - 1);
-     };
+    const handleBack = () => {
+        resetBackText();
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
 
-     const handleSkip = () => {
-          if (!isStepOptional(activeStep)) {
-               // You probably want to guard against something like this,
-               // it should never occur unless someone's actively trying to break something.
-               throw new Error("You can't skip a step that isn't optional.");
-          }
+    const handleSkip = () => {
+        if (!isStepOptional(activeStep)) {
+            // You probably want to guard against something like this,
+            // it should never occur unless someone's actively trying to break something.
+            throw new Error("You can't skip a step that isn't optional.");
+        }
 
-          resetBackText();
-          setActiveStep((prevActiveStep) => prevActiveStep + 1);
-          setSkipped((prevSkipped) => {
-               const newSkipped = new Set(prevSkipped.values());
-               newSkipped.add(activeStep);
-               return newSkipped;
-          });
-     };
+        resetBackText();
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        setSkipped((prevSkipped) => {
+            const newSkipped = new Set(prevSkipped.values());
+            newSkipped.add(activeStep);
+            return newSkipped;
+        });
+    };
 
-     const handleReset = () => {
-          setActiveStep(0);
-          resetBackText();
-     };
+    const handleReset = () => {
+        setActiveStep(0);
+        resetBackText();
+    };
 
-     return (
-          <SimpleForm submitOnEnter={false} redirect="show" toolbar={
-               <StepToolbar 
-                    active={activeStep}
-                    optional={isStepOptional(activeStep)}
-                    stepCount={props.children.length}
-                    handleBack={handleBack}
-                    handleNext={handleNext}
-                    handleSkip={handleSkip} 
-                    backText={backText}   
-                    validator={validator} 
-                    create={props.create}           
-               />
-          } {...props} validation={props.validate}>
-               <StepHeader 
-                    active={activeStep}
-                    children={props.children}
-                    setStepOptional={setStepOptional}
-                    isStepSkipped={isStepSkipped}
-               />
-               {React.cloneElement(props.children[activeStep], {
-                    setValidator: setValidator,
-                    setBackText: setBackText
-               })}
-          </SimpleForm>
-     )
+    return (
+        <SimpleForm submitOnEnter={false} redirect="show" toolbar={
+            <StepToolbar
+                active={activeStep}
+                optional={isStepOptional(activeStep)}
+                stepCount={props.children.length}
+                handleBack={handleBack}
+                handleNext={handleNext}
+                handleSkip={handleSkip}
+                backText={backText}
+                validator={validator}
+                create={props.create}
+            />
+        } {...props} validation={props.validate}>
+            <StepHeader
+                active={activeStep}
+                children={props.children}
+                setStepOptional={setStepOptional}
+                isStepSkipped={isStepSkipped}
+            />
+            {React.cloneElement(props.children[activeStep], {
+                setValidator: setValidator,
+                setBackText: setBackText
+            })}
+        </SimpleForm>
+    )
 }
