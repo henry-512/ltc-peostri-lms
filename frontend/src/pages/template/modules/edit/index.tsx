f8f0c47ad3bdce3a8c879decca2184f28ba80723
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-03-22 13:55:25 -0400

    Module Template CRUD

--- frontend/src/pages/template/modules/edit/index.tsx
+++ frontend/src/pages/template/modules/edit/index.tsx
@@ -1,50 +1,23 @@
-import { makeStyles } from "@material-ui/core";
-import { Edit, useTranslate } from "react-admin";
-import Stepper from "../../../../components/stepper/Stepper";
-import General from "../steps/General";
-import Modules from "../steps/Modules";
+import { Edit, SimpleForm, useTranslate } from "react-admin";
+import { ModuleTemplateFields, TemplateToolbar } from "src/components/templates";
 import transformer from "../transformer";
-import validateProject from "../validation";
-
-const useStyles = makeStyles(theme => ({
-     root: {},
-     content: {
-          marginTop: theme.spacing(2)
-     },
-     usersTitle: {
-          display: 'flex', 
-          alignItems: 'center'
-     },
-     taskBox: {
-          font: 'inherit'
-     },
-     fieldTitle: {
-          borderBottom: '2px solid ' + theme.palette.primary.main,
-          paddingBottom: '.25rem',
-          lineHeight: '1',
-          color: theme.palette.text.primary,
-          marginBottom: '.25rem'
-     },
-     alignCenter: {
-          alignItems: 'center'
-     }
-}));
+import validateModuleTemplate from "../validation";
 
 const ModuleTemplateEdit = (props: any) => {
      const translate = useTranslate();
-     const classes = useStyles();
 
      return (
           <Edit title={translate('project.edit.title')} {...props} transform={transformer}>
-               <Stepper validate={validateProject} {...props}>
-
-                    <General classes={classes} title={translate('project.steps.general')} style={{ width: "100%" }} validator="general" {...props}/>
-
-                    <Modules classes={classes} title={translate('project.steps.modules')} className={classes.content} validator="modules" {...props}/>
-
-                    {/*<Review classes={classes} title={translate('project.steps.review')} className={classes.content} validator="" {...props}/>*/}
-
-               </Stepper>
+               <SimpleForm
+                validate={validateModuleTemplate}
+                toolbar={
+                    <TemplateToolbar
+                        create={true}
+                    />
+                }
+            >
+                <ModuleTemplateFields getSource={(src: string) => src} />
+            </SimpleForm>
           </Edit>
      )
 }
