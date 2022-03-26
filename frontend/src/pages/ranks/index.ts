08c4da701ee665fa8281186d947b16d36320a8c0
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-03-26 19:45:30 -0400

    Removed unused ranks files

--- frontend/src/pages/ranks/index.ts
+++
@@ -1,8 +0,0 @@
-import PermissionCreate from './create'
-import PermissionEdit from './edit'
-import PermissionList from './list'
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
-
-const PermissionIcon = VerifiedUserIcon;
-
-export { PermissionCreate, PermissionEdit, PermissionList, PermissionIcon };
