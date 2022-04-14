f8073d963c4fac7d14c838f7f8464ecdf3d93371
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-04-14 01:12:20 -0400

    Fix theme extension

--- frontend/src/material-ui.d.ts
+++ frontend/src/material-ui.d.ts
@@ -1,12 +1,12 @@
+import { PaletteOptions, Palette } from "@mui/material/styles/createPalette";
+
 // PATCH for TS and the Theme to add custom colors
 
 declare module '@mui/material/styles/createPalette' {
-    {
-        interface Palette {
-            borderColor?: Palette['primary'];
-        }
-        interface PaletteOptions {
-            borderColor?: PaletteOptions['primary'];
-        }
+    export interface Palette {
+        borderColor?: Palette['primary'];
+    }
+    export interface PaletteOptions {
+        borderColor?: PaletteOptions['primary'];
     }
 }
