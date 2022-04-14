ea391e9ac2f5d2fe57e6ed36054292c5253f7ef6
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-04-14 10:42:38 -0400

    Style check

--- frontend/src/packages/NotificationsWidget/NotificationsEmpty.tsx
+++ frontend/src/packages/NotificationsWidget/NotificationsEmpty.tsx
@@ -9,7 +9,6 @@ const classes = {
     fontSizeLarge: `${PREFIX}-fontSizeLarge`
 };
 
-// TODO jss-to-styled codemod: The Fragment root was replaced by div. Change the tag if needed.
 const Root = styled('div')(({ theme }) => ({
     [`& .${classes.fontSizeLarge}`]: {
         fontSize: '48px'
